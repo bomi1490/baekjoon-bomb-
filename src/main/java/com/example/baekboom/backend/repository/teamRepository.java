aa3936347c18426690927151550c0e330a76f3b9
pys-choi <chlrltjd5263@naver.com>
2023-02-11 13:14:26 +0900

    # [기성 4] dao 및 entity 확인
    1. entity와 dao 몇가지 구현했습니다

--- src/main/java/com/example/baekboom/backend/repository/teamRepository.java
+++ src/main/java/com/example/baekboom/backend/repository/teamRepository.java
@@ -4,6 +4,4 @@ import com.example.baekboom.backend.entity.TeamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface teamRepository extends JpaRepository<TeamEntity, Long> {
-
-    TeamEntity findby
 }
