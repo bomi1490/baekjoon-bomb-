14f07c38bf433cb23e97a83fe746a379207040c5
pys-choi <chlrltjd5263@naver.com>
2023-03-08 21:02:50 +0900

    # [기성 9] controller API 설정 추가

--- src/main/java/com/example/baekboom/backend/repository/problemRepository.java
+++ src/main/java/com/example/baekboom/backend/repository/problemRepository.java
@@ -4,6 +4,7 @@ import com.example.baekboom.backend.entity.ProblemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,4 +14,7 @@ public interface problemRepository extends JpaRepository<ProblemEntity, Long> {
 
     List<ProblemEntity> findAllByLevelAndUser_UserId(Long level, String user_id); // 크롤링할 때 사용하여 동일한 level을 제거
 
+
+    // start와 end 사이에 이벤트를 검색한다.
+    List<ProblemEntity> findByUserIdAndEventTimeBetween(String user_id, LocalDateTime startTime, LocalDateTime endTime);
 }
