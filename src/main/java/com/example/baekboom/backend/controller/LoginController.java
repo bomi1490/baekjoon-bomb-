1a27e1ba3c07ffc1f89b507901d33aa8470ee7bc
jiwonson1 <jiwonson.dev@gmail.com>
2023-03-11 15:41:18 +0900

    # [지원 2] security, jwt, filter 추가

--- src/main/java/com/example/baekboom/backend/controller/LoginController.java
+++ src/main/java/com/example/baekboom/backend/controller/LoginController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import com.example.baekboom.backend.dto.TokenInfo;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@RequestMapping("/members")
 public class LoginController {
 
     private final TokenService tokenService;
@@ -30,12 +32,20 @@ public class LoginController {
 
 
     // 만약 안되면 map이 아니라 String으로 바꿔보기
-    @GetMapping("/member")
+  /*  @GetMapping("/member")
     public Map<String, String> register(@RequestBody String token, @RequestParam String inputId){
         Map<String, String> map = new HashMap<>();
         String team_code = loginService.login_teamName(inputId);
         map.put("inputCode", team_code);
 
         return map;
+    }*/
+
+    @PostMapping("/login")
+    public TokenInfo login(@RequestBody MemberLoginRequestDto memberLoginRequestDto) {
+        String memberId = memberLoginRequestDto.getMemberId();
+        String password = memberLoginRequestDto.getPassword();
+        TokenInfo tokenInfo = memberService.login(memberId, password);
+        return tokenInfo;
     }
 }
--- src/main/java/com/example/baekboom/backend/service/LoginService.java
+++ src/main/java/com/example/baekboom/backend/service/LoginService.java
@@ -1,17 +1,25 @@
 package com.example.baekboom.backend.service;
 
 
+import com.example.baekboom.backend.config.JwtTokenProvider;
 import com.example.baekboom.backend.dao.memberDao;
 import com.example.baekboom.backend.dto.memberDto;
 import com.example.baekboom.backend.dto.memberRankDto;
+import com.example.baekboom.backend.dto.TokenInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class LoginService {
     private memberDao memberDao;
 
@@ -29,6 +37,21 @@ public class LoginService {
 
         public lst_rank getLst_rank() {
             return this.lst_rank;
+        }
+
+        public TokenInfo login(String memberId, String password) {
+            // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
+            // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberId, password);
+
+            // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
+            // authenticate 매서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드가 실행
+            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+            // 3. 인증 정보를 기반으로 JWT 토큰 생성
+            TokenInfo tokenInfo = JwtTokenProvider.generateToken(authentication);
+
+            return tokenInfo;
         }
     }
 
